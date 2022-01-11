2fa2f6e7a13e3207d5d1380d41814f4d2a827c3f
bfiskers <57647811+bfiskers@users.noreply.github.com>
2022-01-11 14:52:04 -0600

    App.js cleanup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useCollection } from "./utilities/data";
 import { findImageUrl } from "./utilities/firebase";
 import ListingUpload from "./components/ListingUpload"
 
-const items = {
-  title: "EcoExchange",
-  items: {
-    clothes: {
-      ListingID: "C101",
-      name: "Jacket",
-      uid: "123" /* user id */,
-      Latitude: "0",
-      Longitude: "0",
-      imageURL:
-        "https://m.media-amazon.com/images/I/51ylk6F0rfL._AC_UY741_.jpg",
-      description: "Lightweigh forest green jacket, lightly used",
-      category: "Clothing",
-      date: "1/19/2021",
-    },
-    food: {
-      ListingID: "F101",
-      name: "Apple",
-      uid: "156",
-      Latitude: "1",
-      Longitude: "1",
-      imageURL:
-        "https://i5.walmartimages.com/asr/7320e63a-de46-4a16-9b8c-526e15219a12_3.e557c1ad9973e1f76f512b34950243a3.jpeg",
-      description: "Red Apple, mint condition",
-      category: "Food",
-      date: "1/1/2021",
-    },
-  },
-};
-
-const categories = {
-  Food: "Food",
-  Clothes: "Clothes",
-  Recreation: "Recreation",
-  Electronics: "Electronics",
-  Other: "Other",
-};
-
 const Banner = ({ title }) => <h1>{title}</h1>;
-
-/*const getCourseTerm = course => (
-    terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-    course.id.slice(1, 4)
-);*/
 
 const Listing = ({ listing }) => {
   const [imageUrl, setImageUrl] = useState("");
@@ -82,15 +36,13 @@ const ListingList = ({ listings }) => (
   </div>
 );
 
-
-
 const App = () => {
   const [listings, loading, error] = useCollection("listings");
   if (loading) return <div>Loading</div>;
   if (error) return <div>Error</div>;
   return (
     <div className="container">
-      <Banner title={items.title} />
+      <Banner title={"EcoExchange"} />
       <ListingList listings={listings} />
       <ListingUpload />
     </div>
