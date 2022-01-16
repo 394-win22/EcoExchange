5cbf4e3ffccc8a6bc2f825487a368abfeea6e3a0
sethdmay <seth@teammay.com>
2022-01-16 13:28:42 -0600

    Corrected distance calc to miles

--- src/utilities/location.js
+++ src/utilities/location.js
@@ -1,15 +1,16 @@
 
+const degreesToMiles = 69.169; // number of miles in a degree
+
 const userToItem = (userLoc, itemLat, itemLong) => {
     return computeDist(userLoc ? userLoc.coords : null, {latitude: itemLat, longitude: itemLong});
 }
 
 const computeDist = (pos1, pos2) => {
     if (!pos1 || !pos2) return 0;
-    console.log(pos1, pos2);
     const latdiff = Math.abs(pos1.latitude - pos2.latitude);
     const longdiff = Math.abs(pos1.longitude - pos2.longitude);
 
-    return Math.round(Math.sqrt(latdiff*latdiff + longdiff*longdiff));
+    return (degreesToMiles * Math.sqrt(latdiff*latdiff + longdiff*longdiff)).toFixed(2);
 };
 
 export { userToItem };
