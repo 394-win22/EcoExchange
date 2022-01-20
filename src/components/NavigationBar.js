d3da3f3c13df116c8bc6d3bdd081721a756f6ad8
Timxuv587 <1430415000@qq.com>
2022-01-20 14:40:01 -0600

    add login

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,38 +10,51 @@ const SignInButton = () => (
       Sign In
     </button>
   );
+
+const SignOutButton = () => (
+    <button className="btn btn-secondary btn-sm"
+        onClick={() => signOut()}>
+      Sign Out
+    </button>
+  );
   
+// const SignInOrOutButton = () => {
+//     const [user] = useUserState();
+//     return (
+//     <div>
+//         { user ? <SignOutButton /> : <SignInButton />}
+//     );
+// };
 
-export default class NavigationBar extends Component{
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-    render(){
-        return(
-            <div>
-                <Navbar bg="light" expand="lg">
+const NavigationBar = () => {
+    
+    const [user] = useUserState();
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
                 <Container>
                     <Navbar.Brand as={Link} to={"/"}>EcoExchange</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
-                        <Nav.Link as={Link} to={"/profile"}>Profile</Nav.Link>
-                        <Nav.Link as={Link} to={"/add-listing"}>Add Listing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                        
-                    </Nav>
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                            <Nav.Link as={Link} to={"/profile"}>Profile</Nav.Link>
+                            <Nav.Link as={Link} to={"/add-listing"}>Add Listing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+
+                                <NavDropdown.Divider />
+
+                            </NavDropdown>
+                                {user ? <SignOutButton /> : <SignInButton />}
+
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
-                </Navbar>
-            </div>
-        )
-    }
+            </Navbar>
+        </div>
+    );
 }
+export default NavigationBar
